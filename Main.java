a9961ac07685894eccdd3abf42f2ccab8fb9ec30
Felix de Pablo <f.depablo.lobo@gmail.com>
2025-05-12 17:17:39 +0200

    creacion de variable

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main{
 		public static void main(String[] args){
 				System.out.println("Hola Mundo GIT");
 				System.out.println("Hola Mundo GIT 2 :)");
+				
+				var variable1 = 3;
+				system.out.println(variable1);
 		}
 		
 		public void saludar(){
